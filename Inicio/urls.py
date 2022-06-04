a77ca327b821570032c496205fe91f8a64635cb1
MonsterSalas <ig.salas@duocuc.cl>
2022-06-03 22:28:57 -0400

    Vista teclado user y teclado admin listos
    Vista teclado user y teclado admin listos

--- Inicio/urls.py
+++ Inicio/urls.py
@@ -7,6 +7,7 @@ from django.conf.urls.static import static
 urlpatterns = [
 
     path('', inicio, name="inicio"),
+    path('indexadmin',inicioadmin,name="indexadmin"),
 
     path('registrar/',registrar_m,name="registrar"),
     path('registrarse/',registrarse,name="registrarse"),
@@ -20,11 +21,15 @@ urlpatterns = [
     #Pag menu admin
     path ('menuadmin/',menuadmin,name="menu_admin"),
     path('micadmin/',micadmin,name="micadmin"),
+    path('tecladoadmin/',tecladoadmin,name="tecladoadmin"),
     #Pagina iniciar
     path('iniciar/',iniciar,name="iniciar"),
     
     #Valida usuario
     path('iniciarsesion/',iniciar_sesion,name ="iniciarsesion"),
+    #Mostrar productos
+    path('teclados',mostrarTeclado,name="teclados"),
+    path('teclados/<int:id>',teclado, name="teclado"),
 
     path('microfonos/',mostrarMic, name="mostrarMic"),
     path('microfonos/<int:id>',micro, name="micro"),
