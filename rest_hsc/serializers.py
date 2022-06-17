a74f72ac95945160b4be9cd59686a7a944114c4f
Hugo Toro <hugo.torozuniga@hotmail.com>
2022-06-17 19:51:02 -0400

    xd
    xd

--- rest_hsc/serializers.py
+++ rest_hsc/serializers.py
@@ -1,7 +1,17 @@
 from rest_framework import serializers
-from Inicio.models import Usuario,Producto,Region
+from Inicio.models import Usuario,Producto,Marca
 
 class UsuarioSerializer(serializers.ModelSerializer):
     class Meta:
         model = Usuario
         fields = ['username','contrasennia','nombre','apellido','email','tipousuario']
+
+class ProductoSerializer(serializers.ModelSerializer):
+    class Meta:
+        model= Producto
+        fields= ['idProducto','nombreProducto','precioProducto','especificacionProd','stockProd','tipoprod','marca']
+
+class MarcaSerializer(serializers.ModelSerializer):
+    class Meta:
+        model= Marca
+        fields=['idMarca','nombreMarca']
--- rest_hsc/urls.py
+++ rest_hsc/urls.py
@@ -1,10 +1,19 @@
 from django.urls import path
-from rest_hsc.views import lista_usuarios,agregarU,controlU
+from rest_hsc.views import lista_usuarios,agregarU,controlU, lista_productos, agregarP,controlP,lista_marcas,agregarM,controlM
 
 urlpatterns = [
+    #Usuarios
     path('lista_usuarios/',lista_usuarios,name="lista_usuarios"),
     path('agregarU/',agregarU,name="agregarU"),
     path('controlU/<usuario1>',controlU,name="controlU"),
+    #Productos
+    path('lista_productos/',lista_productos,name="lista_productos"),
+    path('agregarP/',agregarP,name="agregarP"),
+    path('controlP/<idproductos>',controlP,name="controlP"),
+    #Marcas
+    path('lista_marcas/',lista_marcas,name="lista_marcas"),
+    path('agregarM/',agregarM,name="agregarM"),
+    path('controlM/<idmarcas>',controlM,name="controlM"),
 
-    
+
 ]
